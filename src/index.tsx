0aa89d753689cc71d8389731a09b079c758513d5
Angkush <angkysahu2502@gmail.com>
2022-07-22 01:16:01 +0530

    removed service worker as it was creating issues for google login

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import { CryptoContext } from "./state";
-import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -11,5 +10,3 @@ root.render(
 		<App />
 	</CryptoContext>,
 );
-
-serviceWorker.register();
